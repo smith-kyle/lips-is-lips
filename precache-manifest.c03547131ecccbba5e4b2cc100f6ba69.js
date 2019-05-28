f82baa06a38902683b5dc34b8bc8dfb2719a0b1d
Kyle Smith <smith.kyle.r93@gmail.com>
2019-05-27 20:00:08 -0700

    Updates

---
+++ precache-manifest.c03547131ecccbba5e4b2cc100f6ba69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175b10b32114ebcf31994c27cb9cec9c",
+    "url": "/lips-is-lips/index.html"
+  },
+  {
+    "revision": "f371533c8f5ab79a08f1",
+    "url": "/lips-is-lips/static/css/main.8d868c3c.chunk.css"
+  },
+  {
+    "revision": "f16ec6ac314e2fe0abbd",
+    "url": "/lips-is-lips/static/js/2.e609851f.chunk.js"
+  },
+  {
+    "revision": "f371533c8f5ab79a08f1",
+    "url": "/lips-is-lips/static/js/main.159caaf4.chunk.js"
+  },
+  {
+    "revision": "a9462a94e15fda36bb1b",
+    "url": "/lips-is-lips/static/js/runtime~main.ca08c8d3.js"
+  }
+]);
